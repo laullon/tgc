bf527ff5e28306d4b47efb6815b45856de4b2947
German Laullon <laullon@gmail.com>
2010-12-13 20:44:00 -0800

    javascript ok

--- tgc_wp_theme/js/script.js
+++ tgc_wp_theme/js/script.js
@@ -1,19 +1,17 @@
 var jq = jQuery.noConflict();
 jq(document).ready(function(){
-    jq("ul#menu-menu-arriba").superfish({
-        //delay:       1000000
-    });
-});
+    jq("ul#menu-menu-arriba").superfish();
 
-jq("a.ir").click(function(){
-    var path=jq(this).attr("href");
-    var id=jq(this).attr("id");
-    var ele=jq("input[name='"+id+"']").attr("value");
-    if(ele){
-        var des=path+ele+"/";
-        jq(this).attr("href",des);
-        return true;
-    }else{
-        return false;
-    }
+    jq("a.ir").click(function(){
+        var path=jq(this).attr("href");
+        var id=jq(this).attr("id");
+        var ele=jq("input[name='"+id+"']").attr("value");
+        if(ele){
+            var des=path+ele+"/";
+            jq(this).attr("href",des);
+            return true;
+        }else{
+            return false;
+        }
+    });
 });
